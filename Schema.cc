09a913bc8a96fb90bbf159eceb8b9681ed50ccc0
D Girish Kumar Reddy <girish.iiith@gmail.com>
2012-01-17 20:07:15 -0500

    Removed trailing spaces for schema

--- Schema.cc
+++ Schema.cc
@@ -6,133 +6,133 @@
 
 int Schema :: Find (char *attName) {
 
-	for (int i = 0; i < numAtts; i++) {
-		if (!strcmp (attName, myAtts[i].name)) {
-			return i;
-		}
-	}
+    for (int i = 0; i < numAtts; i++) {
+        if (!strcmp (attName, myAtts[i].name)) {
+            return i;
+        }
+    }
 
-	// if we made it here, the attribute was not found
-	return -1;
+    // if we made it here, the attribute was not found
+    return -1;
 }
 
 Type Schema :: FindType (char *attName) {
 
-	for (int i = 0; i < numAtts; i++) {
-		if (!strcmp (attName, myAtts[i].name)) {
-			return myAtts[i].myType;
-		}
-	}
+    for (int i = 0; i < numAtts; i++) {
+        if (!strcmp (attName, myAtts[i].name)) {
+            return myAtts[i].myType;
+        }
+    }
 
-	// if we made it here, the attribute was not found
-	return Int;
+    // if we made it here, the attribute was not found
+    return Int;
 }
 
 int Schema :: GetNumAtts () {
-	return numAtts;
+    return numAtts;
 }
 
 Attribute *Schema :: GetAtts () {
-	return myAtts;
+    return myAtts;
 }
 
 Schema :: Schema (char *fName, char *relName) {
 
-	FILE *foo = fopen (fName, "r");
-	
-	// this is enough space to hold any tokens
-	char space[200];
+    FILE *foo = fopen (fName, "r");
 
-	fscanf (foo, "%s", space);
-	int totscans = 1;
+    // this is enough space to hold any tokens
+    char space[200];
 
-	// see if the file starts with the correct keyword
-	if (strcmp (space, "BEGIN")) {
-		cout << "Unfortunately, this does not seem to be a schema file.\n";
-		exit (1);
-	}	
-		
-	while (1) {
+    fscanf (foo, "%s", space);
+    int totscans = 1;
 
-		// check to see if this is the one we want
-		fscanf (foo, "%s", space);
-		totscans++;
-		if (strcmp (space, relName)) {
+    // see if the file starts with the correct keyword
+    if (strcmp (space, "BEGIN")) {
+        cout << "Unfortunately, this does not seem to be a schema file.\n";
+        exit (1);
+    }
 
-			// it is not, so suck up everything to past the BEGIN
-			while (1) {
+    while (1) {
 
-				// suck up another token
-				if (fscanf (foo, "%s", space) == EOF) {
-					cerr << "Could not find the schema for the specified relation.\n";
-					exit (1);
-				}
+        // check to see if this is the one we want
+        fscanf (foo, "%s", space);
+        totscans++;
+        if (strcmp (space, relName)) {
 
-				totscans++;
-				if (!strcmp (space, "BEGIN")) {
-					break;
-				}
-			}
+            // it is not, so suck up everything to past the BEGIN
+            while (1) {
 
-		// otherwise, got the correct file!!
-		} else {
-			break;
-		}
-	}
+                // suck up another token
+                if (fscanf (foo, "%s", space) == EOF) {
+                    cerr << "Could not find the schema for the specified relation.\n";
+                    exit (1);
+                }
 
-	// suck in the file name
-	fscanf (foo, "%s", space);
-	totscans++;
-	fileName = strdup (space);
+                totscans++;
+                if (!strcmp (space, "BEGIN")) {
+                    break;
+                }
+            }
 
-	// count the number of attributes specified
-	numAtts = 0;
-	while (1) {
-		fscanf (foo, "%s", space);
-		if (!strcmp (space, "END")) {
-			break;		
-		} else {
-			fscanf (foo, "%s", space);
-			numAtts++;
-		}
-	}
+            // otherwise, got the correct file!!
+        } else {
+            break;
+        }
+    }
 
-	// now actually load up the schema
-	fclose (foo);
-	foo = fopen (fName, "r");
+    // suck in the file name
+    fscanf (foo, "%s", space);
+    totscans++;
+    fileName = strdup (space);
 
-	// go past any un-needed info
-	for (int i = 0; i < totscans; i++) {
-		fscanf (foo, "%s", space);
-	}
+    // count the number of attributes specified
+    numAtts = 0;
+    while (1) {
+        fscanf (foo, "%s", space);
+        if (!strcmp (space, "END")) {
+            break;
+        } else {
+            fscanf (foo, "%s", space);
+            numAtts++;
+        }
+    }
 
-	// and load up the schema
-	myAtts = new Attribute[numAtts];
-	for (int i = 0; i < numAtts; i++ ) {
+    // now actually load up the schema
+    fclose (foo);
+    foo = fopen (fName, "r");
 
-		// read in the attribute name
-		fscanf (foo, "%s", space);	
-		myAtts[i].name = strdup (space);
+    // go past any un-needed info
+    for (int i = 0; i < totscans; i++) {
+        fscanf (foo, "%s", space);
+    }
 
-		// read in the attribute type
-		fscanf (foo, "%s", space);
-		if (!strcmp (space, "Int")) {
-			myAtts[i].myType = Int;
-		} else if (!strcmp (space, "Double")) {
-			myAtts[i].myType = Double;
-		} else if (!strcmp (space, "String")) {
-			myAtts[i].myType = String;
-		} else {
-			cout << "Bad attribute type for " << myAtts[i].name << "\n";
-			exit (1);
-		}
-	}
+    // and load up the schema
+    myAtts = new Attribute[numAtts];
+    for (int i = 0; i < numAtts; i++ ) {
 
-	fclose (foo);
+        // read in the attribute name
+        fscanf (foo, "%s", space);
+        myAtts[i].name = strdup (space);
+
+        // read in the attribute type
+        fscanf (foo, "%s", space);
+        if (!strcmp (space, "Int")) {
+            myAtts[i].myType = Int;
+        } else if (!strcmp (space, "Double")) {
+            myAtts[i].myType = Double;
+        } else if (!strcmp (space, "String")) {
+            myAtts[i].myType = String;
+        } else {
+            cout << "Bad attribute type for " << myAtts[i].name << "\n";
+            exit (1);
+        }
+    }
+
+    fclose (foo);
 }
 
 Schema :: ~Schema () {
-	delete [] myAtts;
-	myAtts = 0;
+    delete [] myAtts;
+    myAtts = 0;
 }
 
